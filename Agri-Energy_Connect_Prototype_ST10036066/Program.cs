91ea4d5580df8b7330d79868283325a8e0341ec8
Azania Aria Ncube <ST10036066@vcconnect.edu.za>
2024-05-22 12:16:27 +0200

    Add project files.

---
+++ Agri-Energy_Connect_Prototype_ST10036066/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agri_Energy_Connect_Prototype_ST10036066
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
